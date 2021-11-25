38ff4d895a9512520aa451bfae163eb06aa93296
hirleog <87393381+hirleog@users.noreply.github.com>
2021-11-25 14:14:38 -0300

    [UPDATE] AJUSTES DE TRANSIÇÕES DE TELA, INCLUSÃO DA TELA DE FAZER RESERVA.

---
+++ app/src/main/java/com/example/hotelhilbert1/connectSQL.java
@@ -0,0 +1,29 @@
+package com.example.hotelhilbert1;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class connectSQL {
+
+    Connection con;
+    @SuppressLint("NewApi")
+    public Connection connectClass() {
+        String ip = "192.168.15.5", port = "1433", db = "HotelHilbert", username = "guihirle", password = "481516";
+
+        StrictMode.ThreadPolicy a = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(a);
+        String ConnectURL = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + ";databasename=" + db + ";user=" + username + ";"+"password=" + password + ";";
+            con = DriverManager.getConnection(ConnectURL);
+        } catch (Exception e) {
+            Log.e("Erros is", e.getMessage());
+        }
+        return con;
+    }
+}
---
+++ app/src/main/java/com/example/hotelhilbert1/sobre_nos.java
@@ -0,0 +1,15 @@
+package com.example.hotelhilbert1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sobre_nos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre_nos);
+        getSupportActionBar().hide();
+    }
+}
